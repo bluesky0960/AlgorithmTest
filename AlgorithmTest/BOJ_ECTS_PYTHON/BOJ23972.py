1f5c13a5f62bc3e87bfb1a6e2155c4dc579fa981
bluesky0960 <bluesky096049@gmail.com>
2022-01-12 17:26:17 +0900

    BOJ etc problem solved

--- AlgorithmTest/BOJ_ECTS_PYTHON/BOJ23972.py
+++ AlgorithmTest/BOJ_ECTS_PYTHON/BOJ23972.py
@@ -1,3 +1,20 @@
 #https://www.acmicpc.net/problem/23972
 
+"""
+(total - k) * n >= total
+ntotal - nk >= total
+-nk >= total - ntotal
+-nk >= (1-n)total
+-nk/(1-n) >= total
+
+"""
+import math
 k, n = map(int, input().split())
+if n==1:
+    print(-1)
+else:
+    money = (n*k*(-1))//(1-n)
+    if (n*k*(-1))%(1-n):
+        money+=1
+    print(money)
+
