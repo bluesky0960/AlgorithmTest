2a9d97fda86015228ec409c653552abd61f9266c
bluesky0960 <bluesky096049@gmail.com>
2022-01-21 11:46:28 +0900

    BOJ Step 12 Clear

--- AlgorithmTest/BOJ_STEP_PYTHON/Step12/BOJ10814.py
+++ AlgorithmTest/BOJ_STEP_PYTHON/Step12/BOJ10814.py
@@ -1,2 +1,13 @@
 #https://www.acmicpc.net/problem/10814
 import sys
+
+n = int(sys.stdin.readline())
+
+a = []
+for i in range(n):
+    a.append([i] + sys.stdin.readline().split())
+
+a.sort(key=lambda x:(int(x[1]), x[0]))
+
+for i in a:
+    print(i[1], i[2])
